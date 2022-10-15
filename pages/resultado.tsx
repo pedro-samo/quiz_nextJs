90d8bdb5e2f29fbbc26a299dffc5f8181fe87fcf
Pedro S Morais <pedro.smorais@localiza.com>
2022-10-15 16:33:16 -0300

    finising project

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -3,21 +3,26 @@ import React from 'react';
 import styles from '../styles/Resultado.module.css';
 
 import { useRouter } from 'next/router';
+import Estatistica from '../components/Estatistica';
+import Botao from '../components/Botao';
 
-const resultado = () => {
+const Resultado = () => {
   const router = useRouter();
   const total = +router.query.total;
   const certas = +router.query.certas;
   const percentual = Math.round((certas / total) * 100);
 
   return (
-    <div className={styles.resulta}>
+    <div className={styles.resultado}>
       <h1>Resultado Final</h1>
-      <div>{total}</div>
-      <div>{certas}</div>
-      <div>{`${percentual}%`}</div>
+      <div style={{ display: 'flex' }}>
+        <Estatistica texto="Perguntas" valor={total} />
+        <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4" />
+        <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#DE6A33" />
+      </div>
+      <Botao href="/" texto="Tentar Novamente" />
     </div>
   );
 };
 
-export default resultado;
+export default Resultado;
